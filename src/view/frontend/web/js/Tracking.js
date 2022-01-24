226a345ceb35cb2ed0980012b13b480cdc2d05e9
Boris Schellekens <b.schellekens@gmail.com>
2022-01-24 19:05:14 +0100

    onLoad

--- src/view/frontend/web/js/Tracking.js
+++ src/view/frontend/web/js/Tracking.js
@@ -7,14 +7,18 @@
 
 define(['jquery'], function($)
 {
-    return function(config)
-    {
-      if (window.checkoutConfig && window.checkoutConfig.quoteData) {
-        window.dataLayer = window.dataLayer || [];
-        window.dataLayer.push({
-          quoteId: window.checkoutConfig.quoteData.entity_id,
-          userId: window.checkoutConfig.quoteData.entity_id
-        })
+  return function(config)
+  {
+    $(document).ready(
+      function() {
+        if (window.checkoutConfig && window.checkoutConfig.quoteData) {
+          window.dataLayer = window.dataLayer || [];
+          window.dataLayer.push({
+            quoteId: window.checkoutConfig.quoteData.entity_id,
+            userId: window.checkoutConfig.quoteData.entity_id
+          })
+        }
       }
-    };
+    );
+  };
 });
